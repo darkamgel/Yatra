1b98ee0bb44c4840489586fade4215f868e1b101
Saharsha Ojha <50448778+imsaharsha@users.noreply.github.com>
2021-06-01 16:53:28 +0545

    Driver Profile
    Driver profile with car view (Driver information need to be added)

---
+++ profile_app/lib/imagescreen.dart
@@ -0,0 +1,16 @@
+
+
+class
+    Imagepage{
+late String name;
+late String image;
+
+Imagepage(
+{
+  required this.name,
+  required this.image,
+}
+    );
+
+// ignore: empty_constructor_bodies
+}
